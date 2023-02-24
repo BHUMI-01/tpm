ce8983d3e95e77ee52f7f6926e52a05ab53c3b29
Lovish-99 <lovish1999varshney@gmail.com>
2023-02-24 15:02:38 +0530

    qymlmsa

--- front-end/src/components/Student/Education/addqualify.js
+++ front-end/src/components/Student/Education/addqualify.js
@@ -7,11 +7,12 @@ import {
   MDBCard,
   MDBCardBody,
   MDBInput,
+  MDBRadio,
 } from "mdb-react-ui-kit";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const Editqualify2 = () => {
+const AddEducat = () => {
   const [qualifyLevel, setqualifyLevel] = useState("");
   const [qualifyName, setqualifyName] = useState("");
   const [passYear, setpassYear] = useState("");
@@ -21,203 +22,214 @@ const Editqualify2 = () => {
   const [gradeSys, setgradeSys] = useState("");
   const [grade, setGrade] = useState("");
 
-  const add_student_qualify = async () => {
-    const studentId = JSON.parse(localStorage.getItem("student"))._id;
-    await fetch("http://localhost:5000/add-qualify", {
-      method: "post",
-      body: JSON.stringify({
-        qualifyLevel,
-        studentId,
-        qualifyName,
-        passYear,
-        board,
-        rollNum,
-        resultStatus,
-        gradeSys,
-        grade,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    // console.warn(result);
+  const setEmpty = () => {
+    setqualifyLevel("");
+    setqualifyName("");
+    setpassYear("");
+    setBoard("");
+    setrollNum("");
+    setResultStatus("");
+    setgradeSys("");
+    setGrade("");
   };
 
-  const save_update = () => {
-    if (localStorage.getItem("stdprofile")) {
-      return (
-        <Link to="/student/stdqualify">
-          <MDBBtn type="submit" onClick={() => add_student_qualify()}>
-            Save
-          </MDBBtn>
-        </Link>
-      );
-    } else {
-      return (
-        <MDBRow>
-          <MDBCol>
-            <MDBBtn type="submit" onClick={add_student_qualify}>
-              Save
-            </MDBBtn>
-          </MDBCol>
-          <MDBCol>
-            <Link to="/student/uploaddoc">
-              <MDBBtn>Next</MDBBtn>
-            </Link>
-          </MDBCol>
-        </MDBRow>
-      );
-    }
+  // const getQualifyDetails = async () => {
+  //   let result = JSON.parse(localStorage.getItem("stdqualify"));
+  //   console.log(result);
+  //   if (!result) {
+  //     setEmpty();
+  //   } else {
+  //     setqualifyLevel(result.qualifyLevel);
+  //     setqualifyName(result.qualifyName);
+  //     setpassYear(result.passYear);
+  //     setBoard(result.board);
+  //     setrollNum(result.rollNum);
+  //     setResultStatus(result.resultStatus);
+  //     setgradeSys(result.gradeSys);
+  //     setGrade(result.grade);
+  //   }
+  // };
+
+  const set_student_qualify = async () => {
+    const profi = JSON.stringify({
+      qualifyLevel,
+      qualifyName,
+      passYear,
+      board,
+      rollNum,
+      resultStatus,
+      gradeSys,
+      grade,
+    });
+    const varii = JSON.parse(localStorage.getItem("stdqualify"));
+    localStorage.setItem("stdqualify", JSON.stringify(
+      [ varii, profi]
+    ));
   };
 
   return (
-    <MDBContainer fluid>
-      <MDBCard className="text-black m-5">
-        <MDBCardBody>
-          <form>
-            <MDBRow>
-              <MDBCol>Qualification Details</MDBCol>
-              <MDBCol>
-                <Link to="/student/stdqualify">
-                  <MDBBtn>Back</MDBBtn>
-                </Link>
-              </MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol>
-                <label>Qualification Level :</label>
-                <select
-                  className="form-control select2"
-                  name="qualification"
-                  id="qualification"
-                  required
-                  aria-hidden="true"
-                  value={qualifyLevel}
-                  onChange={(e) => setqualifyLevel(e.target.value)}
-                >
-                  <option value="">Please select</option>
-                  <option value="High School">High School</option>
-                  <option value="Intermediate">Intermediate</option>
-                  <option value="Diploma">Diploma</option>
-                  <option value="B.Tech">
-                    B.Tech
-                  </option>
-                  <option value="M.Tech">M.Tech</option>
-                </select>
-              </MDBCol>
-              <MDBCol>
-                <label>Course Name</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={qualifyName}
-                  onChange={(e) => setqualifyName(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
-              <MDBCol>
-                <label>Year Of Passing:</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={passYear}
-                  maxLength="4"
-                  pattern="[0-9]+"
-                  onChange={(e) => setpassYear(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-              <MDBCol>
-                <label>Roll No :</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={rollNum}
-                  pattern="[0-9]*[a-zA-Z]*[0-9]+"
-                  onChange={(e) => setrollNum(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
-              <MDBCol>
-                <label>Board :</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={board}
-                  onChange={(e) => setBoard(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-              <MDBCol>
-                <label>Result :</label>
-                <select
-                  className="form-control select2"
-                  name="result"
-                  id="result"
-                  required
-                  aria-hidden="true"
-                  value={resultStatus}
-                  onChange={(e) => setResultStatus(e.target.value)}
-                >
-                  <option value="">Please select</option>
-                  <option value="Passed">Passed</option>
-                  <option value="Failed">Failed</option>
-                  <option value="Awaited">Awaited</option>
-                </select>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
-              <MDBCol>
-                <label>Grading System :</label>
-                <select
-                  className="form-control select2"
-                  name="grading system"
-                  id="grading system"
-                  required
-                  aria-hidden="true"
-                  value={gradeSys}
-                  onChange={(e) => setgradeSys(e.target.value)}
-                >
-                  <option value="">Please select</option>
-                  <option value="CPI">CPI</option>
-                  <option value="CGPA">CGPA</option>
-                  <option value="Percentage">Percentage</option>
+    <>
+      <MDBContainer fluid>
+        <MDBCard className="text-black m-5">
+          <MDBCardBody>
+            <form>
+              <MDBRow>
+                <MDBCol>Qualification Details</MDBCol>
+              </MDBRow>
+              <hr />
+              <MDBRow>
+                <MDBCol>
+                  <label>Qualification Level :</label>
+                  <select
+                    className="form-control select2"
+                    name="qualification"
+                    id="qualification"
+                    required
+                    aria-hidden="true"
+                    value={qualifyLevel}
+                    onChange={(e) => setqualifyLevel(e.target.value)}
+                  >
+                    <option value="">Please select</option>
+                    <option value="High School">High School</option>
+                    <option value="Intermediate">Intermediate</option>
+                    <option value="Diploma">Diploma</option>
+                    <option value="Undergraduation(B.Tech)">
+                      Undergraduation(B.Tech)
+                    </option>
+                    <option value="Graduation(M.Tech)">Graduation(M.Tech)</option>
                   </select>
-              </MDBCol>
-              <MDBCol>
-                <label>Grade / % :</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={grade}
-                  pattern="[0-9]+"
-                  onChange={(e) => setGrade(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-            </MDBRow>
+                </MDBCol>
+                <MDBCol>
+                  <label>Course Name</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={qualifyName}
+                    onChange={(e) => setqualifyName(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+              </MDBRow>
 
-            <MDBRow style={{ height: "20px" }}></MDBRow>
+              <MDBRow style={{ height: "20px" }}></MDBRow>
 
-            <MDBRow>{save_update()}</MDBRow>
-          </form>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBContainer>
+              <MDBRow>
+                <MDBCol>
+                  <label>Year Of Passing:</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={passYear}
+                    maxLength="4"
+                    pattern="[0-9]+"
+                    onChange={(e) => setpassYear(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+                <MDBCol>
+                  <label>Roll No :</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={rollNum}
+                    pattern="[0-9]*[a-zA-Z]*[0-9]+"
+                    onChange={(e) => setrollNum(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBCol>
+                  <label>Board :</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={board}
+                    onChange={(e) => setBoard(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+                <MDBCol>
+                  <label>Result :</label>
+                  <select
+                    className="form-control select2"
+                    name="result"
+                    id="result"
+                    required
+                    aria-hidden="true"
+                    value={resultStatus}
+                    onChange={(e) => setResultStatus(e.target.value)}
+                  >
+                    <option value="">Please select</option>
+                    <option value="Passed">Passed</option>
+                    <option value="Failed">Failed</option>
+                    <option value="Awaited">Awaited</option>
+                  </select>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBCol>
+                  <label>Grading System :</label>
+                  <select
+                    className="form-control select2"
+                    name="grading system"
+                    id="grading system"
+                    required
+                    aria-hidden="true"
+                    value={gradeSys}
+                    onChange={(e) => setgradeSys(e.target.value)}
+                  >
+                    <option value="">Please select</option>
+                    <option value="CPI">CPI</option>
+                    <option value="CGPA">CGPA</option>
+                    <option value="Percentage">Percentage</option>
+                  </select>
+                </MDBCol>
+                <MDBCol>
+                  <label>Grade / % :</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={grade}
+                    pattern="[0-9]+"
+                    onChange={(e) => setGrade(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBRow>
+                  <MDBCol>
+                    <MDBBtn
+                      type="submit"
+                      onClick={() => {
+                        set_student_qualify();
+                
+                      }}
+                    >
+                      Save
+                    </MDBBtn>
+                  </MDBCol>
+                  <MDBCol>
+                    <Link to="/student/uploaddoc"><MDBBtn type="submit">Next</MDBBtn></Link>
+                  </MDBCol>
+                </MDBRow>
+              </MDBRow>
+            </form>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
+    </>
   );
 };
 
-export default Editqualify2;
+export default AddEducat;
--- front-end/src/containers/Student/AddEducat.js
+++ front-end/src/containers/Student/AddEducat.js
@@ -7,11 +7,14 @@ import {
   MDBCard,
   MDBCardBody,
   MDBInput,
+  MDBRadio,
 } from "mdb-react-ui-kit";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import AddQualify from "../../components/Student/Education/addqualify";
 
 const AddEducat = () => {
+  const [show, setShow] = useState(false);
   const [qualifyLevel, setqualifyLevel] = useState("");
   const [qualifyName, setqualifyName] = useState("");
   const [passYear, setpassYear] = useState("");
@@ -71,165 +74,178 @@ const AddEducat = () => {
   };
 
   return (
-    <MDBContainer fluid>
-      <MDBCard className="text-black m-5">
-        <MDBCardBody>
-          <form>
-            <MDBRow>
-              <MDBCol>Qualification Details</MDBCol>
-            </MDBRow>
-            <hr />
-            <MDBRow>
-              <MDBCol>
-                <label>Qualification Level :</label>
-                <select
-                  className="form-control select2"
-                  name="qualification"
-                  id="qualification"
-                  required
-                  aria-hidden="true"
-                  value={qualifyLevel}
-                  onChange={(e) => setqualifyLevel(e.target.value)}
-                >
-                  <option value="">Please select</option>
-                  <option value="High School">High School</option>
-                  <option value="Intermediate">Intermediate</option>
-                  <option value="Diploma">Diploma</option>
-                  <option value="Undergraduation(B.Tech)">
-                    Undergraduation(B.Tech)
-                  </option>
-                  <option value="Graduation(M.Tech)">Graduation(M.Tech)</option>
-                </select>
-              </MDBCol>
-              <MDBCol>
-                <label>Course Name</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={qualifyName}
-                  onChange={(e) => setqualifyName(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
-              <MDBCol>
-                <label>Year Of Passing:</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={passYear}
-                  maxLength="4"
-                  pattern="[0-9]+"
-                  onChange={(e) => setpassYear(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-              <MDBCol>
-                <label>Roll No :</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={rollNum}
-                  pattern="[0-9]*[a-zA-Z]*[0-9]+"
-                  onChange={(e) => setrollNum(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
-              <MDBCol>
-                <label>Board :</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={board}
-                  onChange={(e) => setBoard(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-              <MDBCol>
-                <label>Result :</label>
-                <select
-                  className="form-control select2"
-                  name="result"
-                  id="result"
-                  required
-                  aria-hidden="true"
-                  value={resultStatus}
-                  onChange={(e) => setResultStatus(e.target.value)}
-                >
-                  <option value="">Please select</option>
-                  <option value="Passed">Passed</option>
-                  <option value="Failed">Failed</option>
-                  <option value="Awaited">Awaited</option>
-                </select>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
-              <MDBCol>
-                <label>Grading System :</label>
-                <select
-                  className="form-control select2"
-                  name="grading system"
-                  id="grading system"
-                  required
-                  aria-hidden="true"
-                  value={gradeSys}
-                  onChange={(e) => setgradeSys(e.target.value)}
-                >
-                  <option value="">Please select</option>
-                  <option value="CPI">CPI</option>
-                  <option value="CGPA">CGPA</option>
-                  <option value="Percentage">Percentage</option>
-                </select>
-              </MDBCol>
-              <MDBCol>
-                <label>Grade / % :</label>
-                <MDBInput
-                  id="enumber"
-                  type="text"
-                  value={grade}
-                  pattern="[0-9]+"
-                  onChange={(e) => setGrade(e.target.value)}
-                  required
-                ></MDBInput>
-              </MDBCol>
-            </MDBRow>
-
-            <MDBRow style={{ height: "20px" }}></MDBRow>
-
-            <MDBRow>
+    <>
+      <MDBContainer fluid>
+        <MDBCard className="text-black m-5">
+          <MDBCardBody>
+            <form>
+              <MDBRow>
+                <MDBCol>Qualification Details</MDBCol>
+              </MDBRow>
+              <hr />
               <MDBRow>
                 <MDBCol>
-                  <MDBBtn
-                    type="submit"
-                    onClick={() => {
-                      set_student_qualify();
-                      getQualifyDetails();
-                    }}
+                  <label>Qualification Level :</label>
+                  <select
+                    className="form-control select2"
+                    name="qualification"
+                    id="qualification"
+                    required
+                    aria-hidden="true"
+                    value={qualifyLevel}
+                    onChange={(e) => setqualifyLevel(e.target.value)}
                   >
-                    Save
-                  </MDBBtn>
+                    <option value="">Please select</option>
+                    <option value="High School">High School</option>
+                    <option value="Intermediate">Intermediate</option>
+                    <option value="Diploma">Diploma</option>
+                    <option value="Undergraduation(B.Tech)">
+                      Undergraduation(B.Tech)
+                    </option>
+                    <option value="Graduation(M.Tech)">Graduation(M.Tech)</option>
+                  </select>
                 </MDBCol>
                 <MDBCol>
-                  <Link to="/student/uploaddoc"><MDBBtn type="submit">Next</MDBBtn></Link>
+                  <label>Course Name</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={qualifyName}
+                    onChange={(e) => setqualifyName(e.target.value)}
+                    required
+                  ></MDBInput>
                 </MDBCol>
               </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBCol>
+                  <label>Year Of Passing:</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={passYear}
+                    maxLength="4"
+                    pattern="[0-9]+"
+                    onChange={(e) => setpassYear(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+                <MDBCol>
+                  <label>Roll No :</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={rollNum}
+                    pattern="[0-9]*[a-zA-Z]*[0-9]+"
+                    onChange={(e) => setrollNum(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBCol>
+                  <label>Board :</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={board}
+                    onChange={(e) => setBoard(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+                <MDBCol>
+                  <label>Result :</label>
+                  <select
+                    className="form-control select2"
+                    name="result"
+                    id="result"
+                    required
+                    aria-hidden="true"
+                    value={resultStatus}
+                    onChange={(e) => setResultStatus(e.target.value)}
+                  >
+                    <option value="">Please select</option>
+                    <option value="Passed">Passed</option>
+                    <option value="Failed">Failed</option>
+                    <option value="Awaited">Awaited</option>
+                  </select>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBCol>
+                  <label>Grading System :</label>
+                  <select
+                    className="form-control select2"
+                    name="grading system"
+                    id="grading system"
+                    required
+                    aria-hidden="true"
+                    value={gradeSys}
+                    onChange={(e) => setgradeSys(e.target.value)}
+                  >
+                    <option value="">Please select</option>
+                    <option value="CPI">CPI</option>
+                    <option value="CGPA">CGPA</option>
+                    <option value="Percentage">Percentage</option>
+                  </select>
+                </MDBCol>
+                <MDBCol>
+                  <label>Grade / % :</label>
+                  <MDBInput
+                    id="enumber"
+                    type="text"
+                    value={grade}
+                    pattern="[0-9]+"
+                    onChange={(e) => setGrade(e.target.value)}
+                    required
+                  ></MDBInput>
+                </MDBCol>
+              </MDBRow>
+
+              <MDBRow style={{ height: "20px" }}></MDBRow>
+
+              <MDBRow>
+                <MDBRow>
+                  <MDBCol>
+                    <MDBBtn
+                      type="submit"
+                      onClick={() => {
+                        set_student_qualify();
+                        getQualifyDetails();
+                      }}
+                    >
+                      Save
+                    </MDBBtn>
+                  </MDBCol>
+                  <MDBCol>
+                    <Link to="/student/uploaddoc"><MDBBtn type="submit">Next</MDBBtn></Link>
+                  </MDBCol>
+                </MDBRow>
+              </MDBRow>
+            </form>
+            <MDBRow>
+              <MDBRadio
+                name="flexRadioDefault"
+                checked={!show}
+                onClick={() => setShow(!show)}
+                style={{ fontsize: "16px", fontweight: "bold" }}
+                id="flexRadioDefault1"
+                label="Add Qualification"
+              />
             </MDBRow>
-          </form>
-        </MDBCardBody>
-      </MDBCard>
-    </MDBContainer>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
+      {show ? null : <AddQualify />}
+    </>
   );
 };
 
