8444baaa1872fb521eaa83d6cb9bd463bc80af0b
Lovish-99 <lovish1999varshney@gmail.com>
2023-03-19 21:06:28 +0530

    hhs

--- front-end/src/components/navbar/Navbar.js
+++ front-end/src/components/navbar/Navbar.js
@@ -30,9 +30,12 @@ function navbar() {
                 <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
                 <div className="menu">
                   <li><Link to="/">Home</Link></li>
-                  {auth? <li><Link to='/stddash'><i className="fa-solid fa-user"></i> {auth.firstName}</Link></li>:null}
-                  {authh? (authh.role ? <li><Link to="/recruiter"><i className="fa-solid fa-user"></i> {authh.username}</Link></li>:<li><Link to="/admin"><i className="fa-solid fa-user"></i> {authh.username}</Link></li>):
-                   null}
+                  {auth ? <li><Link to='/stddash'><i className="fa-solid fa-user"></i> {auth.firstName}</Link></li> : null}
+                  {authh ? (authh.role === "recruiter" ?
+                    <li><Link to="/recruiter"><i className="fa-solid fa-user"></i> {authh.username}</Link></li>
+                    :
+                    <li><Link to="/admin"><i className="fa-solid fa-user"></i> {authh.username}</Link></li>) :
+                    null}
                   <li><Link onClick={() => logout()} to="/register">Logout</Link></li>
                 </div>
               </ul>
--- front-end/src/containers/Student/Student.js
+++ front-end/src/containers/Student/Student.js
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import {
-    MDBContainer,
     MDBRow,
     MDBCol,
-    MDBBtnGroup
+    MDBBtnGroup,
+    MDBCard,
+    MDBCardBody
 } from 'mdb-react-ui-kit';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
+import Side from '../../components/Sidebar';
 
 function Student() {
     const authorize = JSON.parse(localStorage.getItem("token"));
@@ -14,53 +16,89 @@ function Student() {
         if (!authorize) {
             navigate('/');
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
+    const colordiv1 = () => {
+        if(localStorage.getItem("stdprofile")){
+            return <div id="myBar" className="w3-green" style={{height: "10px", backgroundColor: "lightblue"}}></div>;
+        }
+        else{
+            return <div id="myBar" className="w3-green" style={{height: "10px"}}></div>;
+        }
+    }
+    const colordiv2 = () => {
+        if(localStorage.getItem("stdperaddress")){
+            return <div id="myBar" className="w3-green" style={{height: "10px", backgroundColor: "lightblue"}}></div>;
+        }
+        else{
+            return <div id="myBar" className="w3-green" style={{height: "10px"}}></div>;
+        }
+    }
+    const colordiv3 = () => {
+        if(localStorage.getItem("stdqualify")){
+            return <div id="myBar" className="w3-green" style={{height: "10px", backgroundColor: "lightblue"}}></div>;
+        }
+        else{
+            return <div id="myBar" className="w3-green" style={{height: "10px"}}></div>;
+        }
+    }
+    const colordiv4 = () => {
+        if(localStorage.getItem("upload")){
+            return <div id="myBar" className="w3-green" style={{height: "10px", backgroundColor: "lightblue"}}></div>;
+        }
+        else{
+            return <div id="myBar" className="w3-green" style={{height: "10px"}}></div>;
+        }
+    }
 
     return (
-        <MDBContainer className="my-5 gradient-form">
-            <MDBRow>
-                <h2 className='text-center bg-success text-bg-dark p-2'>Student PANEL</h2>
-                <MDBRow>
-                    <h2 style={{ height: "40px" }}></h2>
-                    <MDBBtnGroup horizontal="true" style={{ padding: "10px" }}>
-                        <MDBCol md={3} style={{ textAlign: "center" }}>
-                            <Link to="/student/stdprofile" >
-                                <i className="fa-regular fa-circle"> Profile</i>
-                            </Link>
-                        </MDBCol>
+        <MDBRow>
+            <MDBCol md={2}>
+                <Side />
+            </MDBCol>
+            <MDBCol>
+                <MDBCard style={{ borderRadius: "0px" }}>
+                    <MDBCardBody>
+                        <h2 className='text-center bg-success text-bg-dark p-2'>Student PANEL</h2>
+                        <MDBRow>
+                            <MDBRow style={{ height: "40px" }}></MDBRow>
+                            <MDBBtnGroup horizontal="true" style={{ padding: "10px" }}>
+                                <MDBCol md={3} style={{ textAlign: "center" }}>
+                                    <i className="fa-regular fa-circle"> Profile</i>
+                                    {colordiv1()}
+                                </MDBCol>
 
-                        <MDBCol md={3} style={{ textAlign: "center" }}>
-                            <Link to="/student/stdaddress" >
-                                <i className="fa-regular fa-circle"> Address</i>
-                            </Link>
-                        </MDBCol>
+                                <MDBCol md={3} style={{ textAlign: "center" }}>
+                                    <i className="fa-regular fa-circle"> Address</i>
+                                    {colordiv2()}
+                                </MDBCol>
 
-                        <MDBCol md={3} style={{ textAlign: "center" }}>
-                            <Link to="/student/stdqualify" >
-                                <i className="fa-regular fa-circle"> Qualification</i>
-                            </Link>
-                        </MDBCol>
+                                <MDBCol md={3} style={{ textAlign: "center" }}>
+                                    <i className="fa-regular fa-circle"> Qualification</i>
+                                    {colordiv3()}
+                                </MDBCol>
 
-                        <MDBCol md={3} style={{ textAlign: "center" }}>
-                            <Link to="/student/uploaddoc" >
-                                <i className="fa-regular fa-circle"> Documents</i>
-                            </Link>
-                        </MDBCol>
+                                <MDBCol md={3} style={{ textAlign: "center" }}>
+                                    <i className="fa-regular fa-circle"> Documents</i>
+                                    {colordiv4()}
+                                </MDBCol>
 
-                    </MDBBtnGroup>
-                </MDBRow>
+                            </MDBBtnGroup>
+                        </MDBRow>
 
-                <MDBRow>
-                    <Outlet>
-                        <MDBCol md={3} style={{ textAlign: "center" }}>
-                            <Link to="/student/stdqualify" >
-                                <i className="fa-regular fa-circle"> Qualification</i>
-                            </Link>
-                        </MDBCol>
-                    </Outlet>
-                </MDBRow>
-            </MDBRow>
-        </MDBContainer>
+                        <MDBRow>
+                            <Outlet>
+                                <MDBCol md={3} style={{ textAlign: "center" }}>
+                                    <Link to="/student/stdqualify" >
+                                        <i className="fa-regular fa-circle"> Qualification</i>
+                                    </Link>
+                                </MDBCol>
+                            </Outlet>
+                        </MDBRow>
+                    </MDBCardBody>
+                </MDBCard>
+            </MDBCol>
+        </MDBRow>
     )
 }
 
